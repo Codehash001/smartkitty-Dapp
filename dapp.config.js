2bc487fd1123ab3e23b2b251f54f0cb223263704
Codehash001 <codehashblockchain@gmail.com>
2022-08-23 20:30:11 +0530

    test

---
+++ dapp.config.js
@@ -0,0 +1,60 @@
+const FORTMATIC_KEY = process.env.NEXT_PUBLIC_FORTMATIC_KEY
+const RPC_URL = process.env.NEXT_PUBLIC_ALCHEMY_RPC_URL
+
+const config = {
+  title: 'SmartKitty Dapp',
+  description: 'Smart Kitty Club minting Dapp',
+  contractAddress: '0x48e13Dba5E6424578DB8e686797365dc13ce40eD',
+  maxMintAmount: 5,
+  presaleMaxMintAmount: 3,
+  price: 0.01 
+}
+
+const onboardOptions = {
+  dappId: process.env.NEXT_PUBLIC_DAPP_ID,
+  networkId: 4, // Rinkeby
+  darkMode: true,
+  walletSelect: {
+    description:'Plaese select a wallet to mint your Smart Kitty',
+    wallets: [
+      { walletName: 'metamask', preferred: true },
+      { walletName: 'coinbase', preferred: true },
+      {
+        walletName: 'walletLink',
+        preferred: true,
+        rpcUrl: RPC_URL,
+        appName: 'SmartKitty Dapp'
+      },
+      {
+        walletName: 'fortmatic',
+        apiKey: FORTMATIC_KEY,
+        preferred: true
+      },
+      { walletName: 'trust', preferred: true, rpcUrl: RPC_URL },
+      { walletName: 'gnosis', preferred: true },
+      { walletName: 'authereum' },
+
+      {
+        walletName: 'ledger',
+        rpcUrl: RPC_URL
+      },
+      {
+        walletName: 'lattice',
+        rpcUrl: RPC_URL,
+        appName: 'SmartKitty Dapp'
+      },
+      {
+        walletName: 'keepkey',
+        rpcUrl: RPC_URL
+      }
+    ]
+  },
+  walletCheck: [
+    { checkName: 'derivationPath' },
+    { checkName: 'accounts' },
+    { checkName: 'connect' },
+    { checkName: 'network' }
+  ]
+}
+
+export { config, onboardOptions }
---
+++ scripts/whitelist.js
@@ -0,0 +1,6 @@
+
+
+module.exports = [
+    '0x858B22340c864D720e916814073E631c18C4118E' //main account
+  ]
+  
